08ff746730c4ef13da73acbe1e935dd3d7d54e93
Gerardo González <wagegosa@gmail.com>
2020-02-20 06:40:54 -0500

    Se cambia nombre del llamado de la clase.

--- config/ClassEstudiantes/classEstudiantes_sel.php
+++ config/ClassEstudiantes/classEstudiantes_sel.php
@@ -1,30 +1,27 @@
  <?php
 
-class Conductor extends DataBase
-{
-	public $ID;
-  public $NOMBRE;
-	public $APELLIDO;
-  public $EMAIL;
-	
-  public function listarConductor()
+  class Estudiantes extends DataBase
   {
-    try
+    public $ID;
+    public $NOMBRE;
+    public $APELLIDO;
+    public $EMAIL;
+
+    public function listarEstudiantes()
     {
-      parent::Conexion();
-      $sql = "SELECT * FROM tredasolutions.estudiante";
-      $qry = $this->dbCon->prepare($sql);
-      $qry->execute();
-      $row = $qry->fetchAll(PDO::FETCH_OBJ);
-      $qry->closeCursor();
-      return $row;
-      $this->dbCon = null;
-    }
-    catch ( PDOException $e )
-    {
-      die("Ha ocurrido un error inesperado en la base de datos.<br>".$e->getMessage());
+      try {
+        parent::Conexion();
+        $sql = "SELECT * FROM tredasolutions.estudiante";
+        $qry = $this->dbCon->prepare($sql);
+        $qry->execute();
+        $row = $qry->fetchAll(PDO::FETCH_OBJ);
+        $qry->closeCursor();
+        return $row;
+        $this->dbCon = null;
+      } catch (PDOException $e) {
+        die("Ha ocurrido un error inesperado en la base de datos.<br>" . $e->getMessage());
+      }
     }
   }
-}
 
-?>
+  ?>
