156906e2eada0a2202f5ab12ea4367a3ca762ab9
Gerardo González <wagegosa@gmail.com>
2020-02-20 10:40:00 -0500

    Se agrega clase para actualizar la DB del estudiante.

--- config/ClassEstudiantes/classEstudiantes_sel.php
+++ config/ClassEstudiantes/classEstudiantes_sel.php
@@ -1,27 +1,30 @@
  <?php
 
-  class Estudiantes extends DataBase
+class Estudiantes extends DataBase
+{
+	public $ID;
+  public $NOMBRE;
+	public $APELLIDO;
+  public $EMAIL;
+	
+  public function listarEstudiantes()
   {
-    public $ID;
-    public $NOMBRE;
-    public $APELLIDO;
-    public $EMAIL;
-
-    public function listarEstudiantes()
+    try
     {
-      try {
-        parent::Conexion();
-        $sql = "SELECT * FROM tredasolutions.estudiante";
-        $qry = $this->dbCon->prepare($sql);
-        $qry->execute();
-        $row = $qry->fetchAll(PDO::FETCH_OBJ);
-        $qry->closeCursor();
-        return $row;
-        $this->dbCon = null;
-      } catch (PDOException $e) {
-        die("Ha ocurrido un error inesperado en la base de datos.<br>" . $e->getMessage());
-      }
+      parent::Conexion();
+      $sql = "SELECT * FROM tredasolutions.estudiante";
+      $qry = $this->dbCon->prepare($sql);
+      $qry->execute();
+      $row = $qry->fetchAll(PDO::FETCH_OBJ);
+      $qry->closeCursor();
+      return $row;
+      $this->dbCon = null;
+    }
+    catch ( PDOException $e )
+    {
+      die("Ha ocurrido un error inesperado en la base de datos.<br>".$e->getMessage());
     }
   }
+}
 
-  ?>
+?>
