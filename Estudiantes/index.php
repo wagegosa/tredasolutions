0f7b74f03d4ec4f9596477db4248abe1dcf5afc6
Gerardo González <wagegosa@gmail.com>
2020-02-20 14:47:27 -0500

    Se modifica boton de agregar estudios.

--- Estudiantes/index.php
+++ Estudiantes/index.php
@@ -85,7 +85,7 @@ $alert = 'Los datos han sido <strong>Almacenados</strong> corrrectamente';
               <td><?= $libro->EMAIL; ?></td>
               <td>
                 <input type="hidden" name="id" id="id" value="<?= $libro->ID;?>">
-                <a href="../Estudios/index.php?a='<?= $libro->ID;?>'" class="btn btn-xs btn-success">Agregar Estudio</a>
+                <a href="../Estudios/index.php?a=<?= $libro->ID;?>" class="btn btn-xs btn-success">Agregar Estudio</a>
                 <a href="./Mod_Estudiante.php?m='<?= $libro->ID;?>'" class="btn btn-xs btn-info">Modificar</a>
               </td>
               </tr>
