9a5c020ec93fc46859f76deedc1e7eec806ead79
Gerardo González <wagegosa@gmail.com>
2020-02-23 19:36:54 -0500

    Se Quita la tabla del index y se deja en este archivo para que ciuando se consulte se muestre.

--- config/ClassEstudios/classEstudio_SelID.php
+++ config/ClassEstudios/classEstudio_SelID.php
@@ -22,21 +22,47 @@ if ($verId != null) {
         WHERE A.Estudiante = $verId";
   $result = mysqli_query($db, $query);
   if ($result != null) {
-    while ($row = mysqli_fetch_array($result)) {
-      echo "<td>".$row['ID']."</td>";
-      echo "<td>".$row['NOMBREESTUDIO']."</td>";
-      echo "<td>".$row['UNIVERSIDAD']."</td>";
-      echo "<td>".$row['ANIO']."</td>";
-      echo "<td>".$row['ESTUDIANTE']."</td>";
-    }
+    echo "<table class='table table-bordered  table-hover table-striped'>";
+      echo "<thead>";
+        echo "<tr>";
+          echo "<th data-filterable'='false'>Nro</th>";
+          echo "<th data-breakpoints='xs sm'>Nombre</th>";
+          echo "<th data-breakpoints='xs sm'>Universidad</th>";
+          echo "<th data-breakpoints='xs sm'>Año</th>";
+          echo "<th data-breakpoints='xs sm'>Estudiante</th>";
+          echo "<th data-breakpoints='xs sm' data-filterable='false'>Acciones</th>";
+        echo "</tr>";
+      echo "</thead>";
+      echo "<tbody class='table-info' >";
+      while ($row = mysqli_fetch_array($result)) {
+        echo "<td>".$row['ID']."</td>";
+        echo "<td>".$row['NOMBREESTUDIO']."</td>";
+        echo "<td>".$row['UNIVERSIDAD']."</td>";
+        echo "<td>".$row['ANIO']."</td>";
+        echo "<td>".$row['ESTUDIANTE']."</td> <br><br>";
+        echo "<td>";
+          echo "<a href='./Mod_Estudiante.php?m=".$row['ID']." class='btn btn-lg btn-warning' role='button'><span class='glyphicon glyphicon-pencil'></span></a>";
+        echo "</td>";
+      }
+      echo "</tbody>";
+    echo "</table>";
   } else {
-    echo "<tr>No hay registros de estudios para este estudiante</tr>";
-    // echo "<pre>";
-    // print_r($result);
-    // echo "</pre>";
-    // exit;
+    echo "<h1>No hay registros de estudios para este estudiante</h1>";
+    echo "<pre>";
+    print_r($result);
+    echo "</pre>";
+    exit;
   }
   
 }
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+
+</body>
+</html>
